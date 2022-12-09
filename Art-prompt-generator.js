c90f2b721d7f789244d9a23b0e4094971a7d7183
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2022-12-09 16:54:39 +0900

    Update Art-prompt-generator.js

--- Art-prompt-generator.js
+++ Art-prompt-generator.js
@@ -1,3 +1,9 @@
+const colorRule=['Analogous','Monochromatic','Triadic','Complementary','Split Complementary', 'Double Split Complementary', 'Tetradic','NONE'];
+const subjectMatter= [{subject:'', name:''}];
+const adjectives =[{subject:'', name:''}];
+const action = [{subject:'', name:''}];
+const place = [{subject:'', name:''}];
 const randomizer= function(subject){
+    let number = Math.floor(Math.random()*11);
 
 }
