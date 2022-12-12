9d5d48e48c38a1ca88ce10dcaad16838105e44d2
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2022-12-12 15:01:23 +0900

    created functions to filter and map data

--- Art-prompt-generator.js
+++ Art-prompt-generator.js
@@ -1,9 +1,15 @@
 const colorRule=['Analogous','Monochromatic','Triadic','Complementary','Split Complementary', 'Double Split Complementary', 'Tetradic','NONE'];
-const subjectMatter= [{subject:'', name:''}];
-const adjectives =[{subject:'', name:''}];
-const action = [{subject:'', name:''}];
+//this is comprised of four choices of subjects "person" "animal" "landscape" "anything"
+const subjectMatter= [{subject:'person', name:'David Bowie'}, {subject:'animal', name:'cat'}, {subject:'landscape', name:'c'}, {subject:'', name:''}];
+const adjectives =[{subject:'landscape', name:'distant'},{subject:'landscape', name:'lovely'}, {subject:'landscape', name:'vast'}, {subject:'landscape', name:'little'}, {subject:'landscape', name:'desolate'}, {subject:'landscape', name:'charming'}, {subject:'landscape', name:'glorious'}, {subject:'landscape', name:'dreary'}, {subject:'landscape', name:'magnificent'}, {subject:'landscape', name:'bleak'}];
+//if subjectMatter is landscape, omit action.
+const action = [{subject:'', name:''}]; 
 const place = [{subject:'', name:''}];
+const numberGen = function(num){return Math.floor(Math.random()*num)};
+//subjectFilter first filters the subject and then maps the name of the filtered objects.
+const subjectFilter = function (query,arr){return arr.filter(obj=>obj.subject===query).map(obj=>obj.name);}
 const randomizer= function(subject){
-    let number = Math.floor(Math.random()*11);
-
+    //the randomizer takes an input of subject and takes an object from each array.
+    //number decides how many subjectMatters are in the prompt. 
+    let number = numberGen(10)+1;
 }
